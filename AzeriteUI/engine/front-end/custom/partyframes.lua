699500fa634373a7cd247dc322d92b9c5c052407
Lars Norberg <lars@larsnorbergofficial.com>
2018-10-15 19:29:57 +0200

    added option to disable party frames

--- AzeriteUI/engine/front-end/custom/partyframes.lua
+++ AzeriteUI/engine/front-end/custom/partyframes.lua
@@ -5,9 +5,13 @@ if (not Core) then
 	return 
 end
 
-local Module = Core:NewModule("UnitFrameParty", "LibDB", "LibEvent", "LibFrame", "LibUnitFrame", "LibStatusBar")
-
+local Module = Core:NewModule("UnitFrameParty", "LibDB", "LibEvent", "LibFrame", "LibUnitFrame")
 local Layout, UnitFrameStyles
+
+-- Default settings
+local defaults = {
+	enablePartyFrames = true
+}
 
 local Style = function(self, unit, id, _, ...)
 	return UnitFrameStyles.StylePartyFrames(self, unit, id, Layout, ...)
@@ -20,6 +24,7 @@ Module.PreInit = function(self)
 end
 
 Module.OnInit = function(self)
+	self.db = self:NewConfig("UnitFrameParty", defaults, "global")
 
 	self.frame = self:CreateFrame("Frame", nil, "UICenter", "SecureHandlerAttributeTemplate")
 	self.frame:SetAttribute("_onattributechanged", [=[
@@ -42,7 +47,35 @@ Module.OnInit = function(self)
 
 	for i = 1,4 do 
 		self.frame[i] = self:SpawnUnitFrame("party"..i, self.frame, Style)
-		--self.frame[i] = self:SpawnUnitFrame("player", "UICenter", Style)
+		--self.frame[i] = self:SpawnUnitFrame("player", self.frame, Style)
 	end 
+
+	local proxy = self:CreateFrame("Frame", nil, "UICenter", "SecureHandlerAttributeTemplate")
+	proxy:SetFrameRef("VisibilityFrame", self.frame)
+	for key,value in pairs(self.db) do 
+		proxy:SetAttribute(key,value)
+	end 
+	proxy:SetAttribute("_onattributechanged", [=[
+		if name then 
+			name = string.lower(name); 
+		end 
+		if (name == "change-enablepartyframes") then 
+			self:SetAttribute("enablePartyFrames", value); 
+			local visibilityFrame = self:GetFrameRef("VisibilityFrame");
+			UnregisterAttributeDriver(visibilityFrame, "state-vis"); 
+			if value then 
+				RegisterAttributeDriver(visibilityFrame, "state-vis", "[@raid6,exists]hide;[group]show;hide")
+				--RegisterAttributeDriver(visibilityFrame, "state-vis", "[@player,exists]show;hide")
+			else 
+				RegisterAttributeDriver(visibilityFrame, "state-vis", "hide")
+			end 
+		end 
+		
+	]=])
+	self.proxyUpdater = proxy
+
 end 
 
+Module.GetSecureUpdater = function(self)
+	return self.proxyUpdater
+end
