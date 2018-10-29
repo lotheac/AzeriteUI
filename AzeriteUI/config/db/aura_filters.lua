04b59b1a1e461cc307dde1556fee12c8cfdfb792
Lars Norberg <lars@larsnorbergofficial.com>
2018-10-29 10:03:19 +0100

    update target aura filter, add nameplate filter

--- AzeriteUI/config/db/aura_filters.lua
+++ AzeriteUI/config/db/aura_filters.lua
@@ -126,162 +126,82 @@ end
 
 filters.target = function(element, isBuff, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
 
+	-- Retrieve filter flags
 	local auraFlags = auraList[spellID]
+	
+	-- Figure out time currently left
 	local timeLeft 
 	if (expirationTime and expirationTime > 0) then 
 		timeLeft = expirationTime - GetTime()
 	end
 
+	-- Stealable and boss auras
 	if (isStealable or isBossDebuff) then 
 		return true 
-	else
+
+	-- Auras on enemies
+	elseif UnitCanAttack("player", unit) then 
 		if InCombatLockdown() then 
 
+			-- Show filtered auras on hostiles
+			if auraFlags then 
+				if (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
+					return unitIsPlayer[unitCaster] 
+				elseif (bit_band(auraFlags, filterFlags.PlayerIsTank) ~= 0) then 
+					return (CURRENT_ROLE == "TANK")
+				else
+					return (bit_band(auraFlags, filterFlags.OnEnemy) ~= 0)
+				end 
+			end 
+
+			-- Show short self-buffs on enemies 
 			if isBuff then 
-				if unitCaster and UnitIsUnit(unit, unitCaster) and UnitCanAttack("player", unitCaster) then 
+				if unitCaster and UnitIsUnit(unit, unitCaster) and UnitCanAttack("player", unit) then 
 					return ((duration and (duration > 0) and (duration < 180)) or (timeLeft and (timeLeft < 180)))
 				end
+			end 
+		else 
 
-				-- Aura list parsing
-				if auraFlags then 
-					if (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
-						return unitIsPlayer[unitCaster] 
-					elseif (bit_band(auraFlags, filterFlags.PlayerIsTank) ~= 0) then 
-						return (CURRENT_ROLE == "TANK")
-					else
-						return (bit_band(auraFlags, filterFlags.OnEnemy) ~= 0)
-					end 
-				end 
-			else 
-				if unitCaster and UnitCanAttack("player", unitCaster) then 
-					if auraFlags then 
-						if (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
-							return unitIsPlayer[unitCaster] 
-						elseif (bit_band(auraFlags, filterFlags.PlayerIsTank) ~= 0) then 
-							return (CURRENT_ROLE == "TANK")
-						else
-							return (bit_band(auraFlags, filterFlags.OnEnemy) ~= 0)
-						end 
-					end 
+			-- Show long/no duration auras out of combat
+			if (not duration) or (duration <= 0) or (duration > 180) or (timeLeft and (timeLeft > 180)) then 
+				return true
+			end 
+		end 
+
+	-- Auras on friends
+	else 
+		if InCombatLockdown() then 
+
+			-- Show filtered auras
+			if auraFlags then 
+				if (bit_band(auraFlags, filterFlags.OnFriend) ~= 0) then 
+					return true
+				elseif (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
+					return unitIsPlayer[unitCaster] 
 				end
 			end 
 
-			-- Short auras by the player
-			if (unitCaster and unitIsPlayer[unitCaster]) and ((duration and (duration > 0) and (duration < 180)) or (timeLeft and (timeLeft < 180))) then
+		else 
+
+			-- Show long/no duration auras out of combat
+			if (not duration) or (duration <= 0) or (duration > 180) or (timeLeft and (timeLeft > 180)) then 
 				return true
-			end
-
-		else 
-
-			if isBuff then 
-				if (unitCaster and unitIsPlayer[unitCaster]) then 
-					if ((duration and (duration > 0) and (duration < 180)) or (timeLeft and (timeLeft < 180))) then
-						if auraFlags then 
-							if (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
-								return true  
-							end
-						end 
-					end
-				end
-			end
-		end  
-	end
-
+			end 
+		end 
+	end 
 end
 
-filters.player2 = function(element, isBuff, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
-
-	local timeLeft 
-	if (expirationTime and expirationTime > 0) then 
-		timeLeft = expirationTime - GetTime()
-	end
+filters.nameplate = function(element, isBuff, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
 
 	local auraFlags = auraList[spellID]
-	if auraFlags then
-
-		-- Always show boss level priorities
-		if (bit_band(auraFlags, filterFlags.PrioBoss) ~= 0) then 
-			return true 
-
-		-- Auras cast by the player
-		elseif (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
+	if auraFlags then 
+		if (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
 			return unitIsPlayer[unitCaster] 
-
-		-- Auras visible on friendly targets (including ourself)
-		elseif (bit_band(auraFlags, filterFlags.OnFriend) ~= 0) then 
-			return UnitIsFriend(unit, "player") and UnitPlayerControlled(unit)
-
-		-- Auras visible on the player frame (with the exception of the player unit in group frames)
-		elseif (bit_band(auraFlags, filterFlags.OnPlayer) ~= 0) then
-			return (unit == "player") and (not element._owner.unitGroup)
-
-		-- Show remaining auras that hasn't specifically been hidden
-		else 
-			return (bit_band(auraFlags, filterFlags.Never) == 0)
-		end 
-
-	else
-		-- Show auras from hostile npc's
-		if (not unitCaster) or (UnitCanAttack("player", unitCaster) and (not UnitPlayerControlled(unitCaster))) then 
-			return ((not isBuff) and (duration and duration < 180))
-
-		-- Show any auras cast by bosses or the player's vehicle
-		elseif (isBossDebuff or (unitCaster == "vehicle")) then
-			return true
-		else
-			-- Just show all short ones remaining
-			return (duration and (duration > 0) and (duration < 180)) or (timeLeft and (timeLeft < 180))
-		end 
-	end
-
-end
-
-filters.target2 = function(element, isBuff, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
-	local auraFlags = auraList[spellID]
-	if auraFlags then
-
-		-- Always show boss level priorities
-		if (bit_band(auraFlags, filterFlags.PrioBoss) ~= 0) then 
-			return true 
-
-		-- Auras cast by the player
-		elseif (bit_band(auraFlags, filterFlags.ByPlayer) ~= 0) then 
-			return unitIsPlayer[unitCaster] 
-		
-		-- Auras visible on friendly targets (including ourself)
-		elseif (bit_band(auraFlags, filterFlags.OnFriend) ~= 0) then 
-			return UnitIsFriend(unit, "player") and UnitPlayerControlled(unit)
-		
-		-- Auras visible on the player frame (with the exception of the player unit in group frames)
-		elseif (bit_band(auraFlags, filterFlags.OnPlayer) ~= 0) then
-			return (unit == "player") and (not element._owner.unitGroup)
-		
-		-- Auras visible when the player is a tank
 		elseif (bit_band(auraFlags, filterFlags.PlayerIsTank) ~= 0) then 
 			return (CURRENT_ROLE == "TANK")
-
-		-- Show remaining auras that hasn't specifically been hidden
-		else 
-			return (bit_band(auraFlags, filterFlags.Never) == 0)
 		end 
-
-	-- Show any auras cast by bosses or the player's vehicle
-	elseif (isBossDebuff or (unitCaster == "vehicle")) then 
-		return true
-
-	-- Hide unknown debuffs from unknown sources  
-	elseif (not isBuff) and (not unitCaster) then 
-		return false
-
-	-- Show unknown self-buffs on hostile targets
-	elseif (UnitCanAttack("player", unit) and (not UnitPlayerControlled(unit))) then 
-		return (not unitCaster or (unitCaster == unit)) or (isBuff and (duration < 3600))
-
-	-- Show unknown short auras not falling into any of the above categories
-	else 
-		return (not unitCaster) or (duration and (duration > 0) and (duration < 180)) or (timeLeft and (timeLeft < 180))
-	end
-end
+	end 
+end 
 
 filters.focus = function(element, isBuff, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
 	return filters.target(element, button, unit, isOwnedByPlayer, name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3)
