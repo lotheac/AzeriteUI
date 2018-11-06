fde6672f82cd8c4049e85faa782870ca9606352d
Lars Norberg <lars@larsnorbergofficial.com>
2018-11-06 19:34:20 +0100

    added switch to toggle HUD castbar when personal resource display is on

--- AzeriteUI/engine/front-end/custom/playerhud.lua
+++ AzeriteUI/engine/front-end/custom/playerhud.lua
@@ -16,6 +16,28 @@ local Style = function(self, unit, id, _, ...)
 	end 
 end
 
+Module.OnEvent = function(self, event, ...)
+	local arg1, arg2 = ...
+	if ((event == "CVAR_UPDATE") and (arg1 == "DISPLAY_PERSONAL_RESOURCE")) then 
+
+		-- Disable cast element if personal resource display is enabled. 
+		-- We follow the event returns here instead of querying the cvar.
+		if (arg2 == "0") then 
+			self.frame:EnableElement("Cast")
+		elseif (arg2 == "1") then 
+			self.frame:DisableElement("Cast")
+		end
+	elseif (event == "VARIABLES_LOADED") then 
+
+		-- Disable cast element if personal resource display is enabled
+		if (GetCVarBool("nameplateShowSelf")) then 
+			self.frame:DisableElement("Cast")
+		else
+			self.frame:EnableElement("Cast")
+		end
+	end 
+end
+
 Module.PreInit = function(self)
 	local PREFIX = Core:GetPrefix()
 	Layout = CogWheel("LibDB"):GetDatabase(PREFIX..": Layout [UnitFramePlayerHUD]", true)
@@ -24,7 +46,19 @@ end
 
 Module.OnInit = function(self)
 	self.frame = self:SpawnUnitFrame("player", "UICenter", Style)
+
+	-- Disable cast element if personal resource display is enabled
+	if (GetCVarBool("nameplateShowSelf")) then 
+		self.frame:DisableElement("Cast")
+	else 
+		self.frame:EnableElement("Cast")
+	end
 end 
+
+Module.OnEnable = function(self)
+	self:RegisterEvent("CVAR_UPDATE", "OnEvent")
+	self:RegisterEvent("VARIABLES_LOADED", "OnEvent")
+end
 
 Module.GetFrame = function(self)
 	return self.frame
