e8063bdd7c17c2a1ae1429306368fdd2c8f69819
Lars Norberg <lars@larsnorbergofficial.com>
2018-11-06 15:34:44 +0100

    added pvp enabled status to supported classifications

--- AzeriteUI/engine/back-end/plugins/unit_classification.lua
+++ AzeriteUI/engine/back-end/plugins/unit_classification.lua
@@ -3,20 +3,28 @@ local _G = _G
 local pairs = pairs
 
 -- WoW API
+local GetPVPTimer = _G.GetPVPTimer
 local UnitClassification = _G.UnitClassification
+local UnitFactionGroup = _G.UnitFactionGroup
+local UnitIsPlayer = _G.UnitIsPlayer
+local UnitIsPVP = _G.UnitIsPVP
+local UnitIsPVPFreeForAll = _G.UnitIsPVPFreeForAll
+local UnitIsPVPSanctuary = _G.UnitIsPVPSanctuary
 local UnitLevel = _G.UnitLevel
 
--- Objects that we're be looking for in the unitframe
-local classificationToObject = {
+-- Objects that we're be looking for in the element
+local objects = {
 	boss = "Boss",
 	elite = "Elite", 
 	minus = "Minus",
 	rare = "Rare", 
 	rareelite = "RareElite",
-	worldboss = "WorldBoss"
+	worldboss = "WorldBoss",
+	horde = "Horde",
+	alliance = "Alliance"
 }
 
--- Replacement classifications in case the unitframe 
+-- Replacement classifications in case the element 
 -- doesn't use worldboss or rareelite. Which mine doesn't!
 local proxies = {
 	rareelite = "rare",
@@ -33,29 +41,73 @@ local Update = function(self, event, unit)
 		element:PreUpdate(unit)
 	end
 
-	local unitLevel = UnitLevel(unit)
-	local unitClassification = (unitLevel and (unitLevel < 1)) and "worldboss" or UnitClassification(unit)
+	-- classification to be used in post updates
+	local classification
+
+	-- PvP always takes priority
+	if UnitIsPVP(unit) then
+		local faction = UnitFactionGroup(unit)
+		if faction then 
+			classification = (faction == "Alliance") and "alliance" or (faction == "Horde") and "horde"
+		end 
+
+	-- Hide for non-pvp enabled players
+	elseif UnitIsPlayer(unit) then 
+		return element:Hide()
+	else
+
+		-- Show classification for boss/rare/elite npcs.
+		local level = UnitLevel(unit)
+		classification = (level and (level < 1)) and "worldboss" or UnitClassification(unit)
+	end 
+
+	-- Return and hide if nothing usable was found
+	local key = classification and objects[classification]
+	if (not key) then 
+		return element:Hide()
+	end 
 
 	-- Add a little system to allow 'boss' to be used instead of 'worldboss' and 'rare' instead of 'rareelite'
-	local object = element[classificationToObject[unitClassification]]
+	local object = element[key]
 	if (not object) then 
-		local proxyClassification = proxies[unitClassification]
-		local proxy = element[classificationToObject[proxyClassification]] 
-		if proxy then 
-			object = proxy 
-			unitClassification = proxyClassification
+		local proxyClassification = proxies[classification]
+		if proxyClassification then 
+			local proxyKey = objects[proxyClassification] 
+			if proxyKey then 
+				local proxy = element[proxyKey] 
+				if proxy then 
+					object = proxy 
+					classification = proxyClassification
+				end 
+			end 
 		end 
 	end 
 
-	for classificiationName,objectName in pairs(classificationToObject) do 
-		local object = element[objectName] 
-		if object then 
-			object:SetShown(classificiationName == unitClassification)
+	-- return and hide if nothing was found
+	if (not classification) then 
+		return element:Hide()
+	end
+
+	local shown
+	for id,key in pairs(objects) do 
+		local object = element[key] 
+		if object then
+			local show = id == classification
+			object:SetShown(show)
+			shown = shown or show 
 		end 
+	end 
+
+	if shown then 
+		if (not element:IsShown()) then 
+			element:Show()
+		end
+	else 
+		return element:Hide()
 	end 
 
 	if element.PostUpdate then 
-		return element:PostUpdate(unit, unitClassification)
+		return element:PostUpdate(unit, classification)
 	end
 end 
 
@@ -74,6 +126,8 @@ local Enable = function(self)
 		element.ForceUpdate = ForceUpdate
 
 		self:RegisterEvent("UNIT_CLASSIFICATION_CHANGED", Proxy)
+		self:RegisterEvent("UNIT_FACTION", Proxy)
+		self:RegisterEvent("ZONE_CHANGED_NEW_AREA", Proxy, true)
 		
 		return true 
 	end
@@ -82,11 +136,15 @@ end
 local Disable = function(self)
 	local element = self.Classification
 	if element then
+		element:Hide()
+
 		self:UnregisterEvent("UNIT_CLASSIFICATION_CHANGED", Proxy)
+		self:UnregisterEvent("UNIT_FACTION", Proxy)
+		self:UnregisterEvent("ZONE_CHANGED_NEW_AREA", Proxy)
 	end
 end 
 
 -- Register it with compatible libraries
 for _,Lib in ipairs({ (CogWheel("LibUnitFrame", true)), (CogWheel("LibNamePlate", true)) }) do 
-	Lib:RegisterElement("Classification", Enable, Disable, Proxy, 1)
+	Lib:RegisterElement("Classification", Enable, Disable, Proxy, 3)
 end 
