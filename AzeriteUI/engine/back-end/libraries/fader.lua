de6f06dddbcb34ff364fa3dc6458cc1b0ab379d7
Lars Norberg <lars@larsnorbergofficial.com>
2018-11-18 10:32:41 +0100

    optimized fade checks

--- AzeriteUI/engine/back-end/libraries/fader.lua
+++ AzeriteUI/engine/back-end/libraries/fader.lua
@@ -1,4 +1,4 @@
-local LibFader = CogWheel:Set("LibFader", 2)
+local LibFader = CogWheel:Set("LibFader", 5)
 if (not LibFader) then	
 	return
 end
@@ -54,15 +54,16 @@ LibFader.frame = LibFader.frame or LibFader:CreateFrame("Frame", nil, "UICenter"
 LibFader.frame._owner = LibFader
 LibFader.STATE = LibFader.STATE -- current public state of the managers
 LibFader.FORCED = LibFader.FORCED -- when true, all managers are forcefully shown
-LibFader.PRIMARY_STATE = LibFader.PRIMARY_STATE -- current macro driven primary state
-LibFader.SECONDARY_STATE = LibFader.SECONDARY_STATE -- current event dependant secondary state
+LibFader.PRIMARY = LibFader.PRIMARY -- current macro driven primary state
+LibFader.SECONDARY = LibFader.SECONDARY -- current event dependant secondary state
 
--- speed!
-local Frame = LibFader.frame
+-- Speed!
 local Data = LibFader.data
 local Objects = LibFader.objects
-local DefaultAlphas = LibFader.defaultAlphas
 
+-- Primary state driver. 
+-- When this reports "peril", events are immediately stopped, 
+-- and onupdate handlers stop running once the "peril" state is fully achieved. 
 local DRIVER = "[@target,exists][@focus,exists][@boss1,exists][@arena1,exists][@party1,exists][@raid1,exists][combat][possessbar][overridebar][vehicleui]peril;safe"
 
 -- TODO: Use the upcoming aura filter rewrite for this.
@@ -77,27 +78,27 @@ local safeDebuffs = {
 	[194958] = true, -- Ashran Deserter
 
 	-- heal cooldowns
+	[178857] = true, -- Contender (Gladiator's Sanctum buff)
 	[ 11196] = true, -- Recently Bandaged
 	[  6788] = true, -- Weakened Soul
-	[178857] = true, -- Contender (Gladiator's Sanctum buff)
 	
 	-- burst cooldowns
-	[264689] = true, -- Fatigued (cannot benefit from Primal Rage or similar)
 	[ 57723] = true, -- Exhaustion from Heroism
+	[264689] = true, -- Fatigued (cannot benefit from Primal Rage or similar) -- added 8.0.1 (?)
+	[ 95809] = true, -- Insanity from Ancient Hysteria
 	[ 57724] = true, -- Sated from Bloodlust
 	[ 80354] = true, -- Temporal Displacement from Time Warp
-	[ 95809] = true, -- Insanity from Ancient Hysteria
 	
 	-- Resources
 	[ 36032] = true, -- Arcane Charges
 	
 	-- Seasonal 
 	[ 26680] = true, -- Adored "You have received a gift of adoration!" 
+	[ 42146] = true, -- Brewfest Racing Ram Aura
 	[ 26898] = true, -- Heartbroken "You have been rejected and can no longer give Love Tokens!"
 	[ 71909] = true, -- Heartbroken "Suffering from a broken heart."
-	[ 69438] = true, -- Sample Satisfaction (some love crap)
-	[ 42146] = true, -- Brewfest Racing Ram Aura
 	[ 43052] = true, -- Ram Fatigue "Your racing ram is fatigued."
+	[ 69438] = true, -- Sample Satisfaction (some love crap)
 	
 	-- WoD weird debuffs 
 	[174958] = true, -- Acid Trail "Riding on the slippery back of a Goren!"  -- added 6.0.1
@@ -118,7 +119,7 @@ local safeDebuffs = {
 	[173661] = true, -- Vivianne 
 
 	-- BfA
-	[271571] = true, -- Ready! (when doing the "Shell Game" world quests)
+	[271571] = true, -- Ready! (when doing the "Shell Game" world quests) -- added 8.0.1
 }
 
 -- Syntax check 
@@ -134,28 +135,29 @@ local check = function(value, num, ...)
 	error(("Bad argument #%d to '%s': %s expected, got %s"):format(num, name, types, type(value)), 3)
 end
 
+local OnUpdate_PreDelay = function(self, elapsed) 
+	return self._owner:OnUpdate_PreDelay(elapsed) 
+end
+
 local OnUpdate = function(self, elapsed) 
 	return self._owner:OnUpdate(elapsed) 
 end
 
-local SetToDefaultAlpha = function(self) 
-	self:SetAlpha(DefaultAlphas[self]) 
+local SetToDefaultAlpha = function(object) 
+	object:SetAlpha(Objects[object]) 
 end
 
-local SetToZeroAlpha = function(self)
-	self:SetAlpha(0)
+local SetToZeroAlpha = function(object)
+	object:SetAlpha(0)
 end 
 
-local SetToProgressAlpha = function(self, progress)
-	self:SetAlpha(DefaultAlphas[self] * progress) 
+local SetToProgressAlpha = function(object, progress)
+	object:SetAlpha(Objects[object] * progress) 
 end
 
 -- Register an object with a fade manager
 LibFader.RegisterObjectFade = function(self, object)
-
-	-- Store the original alpha, and initiate the fading
-	DefaultAlphas[object] = object:GetAlpha()
-	Objects[object] = true
+	Objects[object] = object:GetAlpha()
 end
 
 -- Unregister an object from a fade manager, and hard reset its alpha
@@ -165,11 +167,10 @@ LibFader.UnregisterObjectFade = function(self, object)
 	end
 
 	-- Retrieve original alpha
-	local alpha = DefaultAlphas[object]
+	local alpha = Objects[object]
 
 	-- Remove the object from the manager
 	Objects[object] = nil
-	DefaultAlphas[object] = nil
 
 	-- Restore the original alpha
 	object:SetAlpha(alpha)
@@ -177,21 +178,22 @@ end
 
 -- Set the default alpha of an opaque object
 LibFader.SetObjectAlpha = function(self, object, alpha)
+	check(alpha, 2, "number")
 	if (not Objects[object]) then 
 		return 
 	end
-	DefaultAlphas[object] = alpha
+	Objects[object] = alpha
 end 
 
 LibFader.CheckMouse = function(self)
 	if SpellFlyout:IsShown() then 
 		Data.mouseOver = true 
-		return 
+		return true
 	end 
 	for object in pairs(Objects) do 
 		if (MouseIsOver(object) and object:IsVisible()) then 
 			Data.mouseOver = true 
-			return  
+			return true
 		end 
 	end 
 	Data.mouseOver = nil
@@ -219,15 +221,14 @@ end
 
 LibFader.CheckAuras = function(self)
 	for i = 1, DEBUFF_MAX_DISPLAY do
-	
-		-- Retrieve debuff information
 		local name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod, value1, value2, value3 = UnitDebuff("player", i, filter)
 
 		-- No name means no more debuffs matching the filter
 		if (not name) then
-			return
+			break
 		end
 
+		-- Set the flag and return if a non-filtered debuff is encountered
 		if (not safeDebuffs[spellId]) then
 			Data.badAura = true
 			return
@@ -266,80 +267,72 @@ LibFader.CheckPower = function(self)
 	Data.lowPower = nil
 end 
 
-LibFader.UpdateState = function(self)
-	if (self.PRIMARY_STATE == "peril") then 
-		self.targetState = "peril"
-	else 
-		self:CheckHealth()
-		self:CheckPower()
-		self:CheckAuras()
-		self:CheckCursor()
-		if (Data.lowHealth or Data.lowPower or Data.badAura or Data.busyCursor) then 
-			self.SECONDARY_STATE = "peril"
-		else 
-			self.SECONDARY_STATE = "safe"
-		end 
-		self.targetState = self.SECONDARY_STATE
-	end 
-	if (self.achievedState ~= self.targetState) then 
-		if (not Frame:GetScript("OnUpdate")) then 
-			Frame:SetScript("OnUpdate", OnUpdate)
+LibFader.UpdatePrimary = function(self, state)
+	if (not state) then 
+		state = self.frame:GetAttribute("state-fade")
+	end
+	if (not state) then 
+		return 
+	end
+	if (self.PRIMARY ~= state) then 
+		self.PRIMARY = state 
+
+		if (state == "peril") then 
+			if self.hasEvents then 
+				self:UnregisterEvent("ZONE_CHANGED_NEW_AREA", "OnEvent")
+				self:UnregisterEvent("UNIT_AURA", "OnEvent")
+				self:UnregisterEvent("UNIT_HEALTH_FREQUENT", "OnEvent")
+				self:UnregisterEvent("UNIT_POWER_FREQUENT", "OnEvent")
+				self:UnregisterEvent("UNIT_DISPLAYPOWER", "OnEvent")
+				self.hasEvents = nil
+			end 
+		else
+			if (not self.hasEvents) then 
+				self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "OnEvent")
+				self:RegisterUnitEvent("UNIT_AURA", "OnEvent", "player")
+				self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "OnEvent", "player") 
+				self:RegisterUnitEvent("UNIT_POWER_FREQUENT", "OnEvent", "player") 
+				self:RegisterUnitEvent("UNIT_DISPLAYPOWER", "OnEvent", "player") 
+				self.hasEvents = true
+			end 
+
+			self:UpdateSecondary()
 		end 
 	end 
 end
 
-LibFader.OnAttributeChanged = function(self, name, value)
-	if (name == "state-fade") then 
-		if (self.PRIMARY_STATE ~= value) then 
-			self.PRIMARY_STATE = value 
+LibFader.UpdateSecondary = function(self)
+	if (self.PRIMARY == "peril") then 
+		return 
+	end 
 
-			-- We only need event handling when the primary state is "safe"
-			if (value == "peril") then 
-				if self.hasEvents then 
-					self:UnregisterEvent("ZONE_CHANGED_NEW_AREA", "OnEvent")
-					self:UnregisterEvent("UNIT_AURA", "OnEvent")
-					self:UnregisterEvent("UNIT_HEALTH_FREQUENT", "OnEvent")
-					self:UnregisterEvent("UNIT_POWER_FREQUENT", "OnEvent")
-					self:UnregisterEvent("UNIT_DISPLAYPOWER", "OnEvent")
-					self.hasEvents = nil
-				end 
-			else
-				if (not self.hasEvents) then 
-					self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "OnEvent")
-					self:RegisterUnitEvent("UNIT_AURA", "OnEvent", "player")
-					self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "OnEvent", "player") 
-					self:RegisterUnitEvent("UNIT_POWER_FREQUENT", "OnEvent", "player") 
-					self:RegisterUnitEvent("UNIT_DISPLAYPOWER", "OnEvent", "player") 
-					self.hasEvents = true
-				end 
-			end 
+	self:CheckHealth()
+	self:CheckPower()
+	self:CheckAuras()
+	self:CheckCursor()
 
-			-- Run a state update to decide final state and update handler
-			self:UpdateState()
-		end 
+	if (Data.lowHealth or Data.lowPower or Data.badAura or Data.busyCursor) then 
+		self.SECONDARY = "peril"
+	else 
+		self.SECONDARY = "safe"
+	end 
+
+	-- Always keep this enabled when primary is "safe"
+	if (not self.frame:GetScript("OnUpdate")) then 
+		self.frame:SetScript("OnUpdate", OnUpdate)
 	end 
 end
 
 LibFader.OnEvent = function(self, event, ...)
 	if (event == "PLAYER_ENTERING_WORLD") then 
 
-		-- Reset all objects to their default alphas
 		self:ForAll(SetToDefaultAlpha)
-
-		-- Initiate delay before first fade-out
-		self.inStartUpLockdown = true 
-		self.totalElapsedStartDelay = 0
-		self.totalDurationStartDelay = 15
-
-		-- Start the update handler
 		self.elapsed = 0
-		self.totalElapsed = 0
-
-		Frame:SetScript("OnUpdate", OnUpdate)
+		self.frame:SetScript("OnUpdate", OnUpdate_PreDelay)
 
 	elseif (event == "PLAYER_LEAVING_WORLD") then
-		Frame:SetScript("OnUpdate", nil)
-		Frame:SetScript("OnAttributeChanged", nil)
+		self.frame:SetScript("OnUpdate", nil)
+		self.frame:SetScript("OnAttributeChanged", nil)
 
 	elseif (event == "UNIT_POWER_FREQUENT" or event == "UNIT_DISPLAYPOWER") then
 		Data.lowPower = self:CheckPower()
@@ -350,8 +343,35 @@ LibFader.OnEvent = function(self, event, ...)
 	elseif (event == "UNIT_AURA") then 
 		Data.badAura = self:CheckAuras()
 	end 
-	self:UpdateState()
+	self:UpdateSecondary()
 end
+
+LibFader.OnUpdate_PreDelay = function(self, elapsed)
+	self.elapsed = self.elapsed + elapsed
+
+	if (self.elapsed < 15) then 
+		return 
+	end
+
+	self.totalElapsed = 0
+	self.totalElapsedIn = 0
+	self.totalElapsedOut = 0
+	self.totalDurationIn = .15
+	self.totalDurationOut = .75
+	self.currentPosition = 1
+	self.achievedState = "peril"
+
+	self.frame:SetScript("OnUpdate", nil)
+	self.frame:SetScript("OnAttributeChanged", function(_, name, value)
+		if (name == "state-fade") then  
+			return self:UpdatePrimary(value) 
+		end
+	end)
+
+	-- Fire off a fake attribute change to initiate fade events
+	self:UpdatePrimary("state-fade", (SecureCmdOptionParse(DRIVER)))
+
+end 
 
 LibFader.OnUpdate = function(self, elapsed)
 	self.elapsed = self.elapsed + elapsed
@@ -361,70 +381,38 @@ LibFader.OnUpdate = function(self, elapsed)
 		return 
 	end 
 
-	-- Initial delay still running?
-	if self.inStartUpLockdown then 
+	-- Shortcut the primary and secondary states
+	local primary = self.PRIMARY == "peril"
+	local secondary = self.SECONDARY == "peril"
+	local mouse = (not primary) and (not secondary) and self:CheckMouse()
 
-		self.totalElapsedStartDelay = self.totalElapsedStartDelay + self.elapsed
-		self.elapsed = 0
+	local toPeril = primary or secondary or mouse
+	local progress = self.elapsed / (toPeril and self.totalDurationIn or self.totalDurationOut)
 
-		if (self.totalElapsedStartDelay < self.totalDurationStartDelay) then 
-			return 
-		end
+	if (primary or secondary or mouse) then 
+		if ((self.currentPosition + progress) < 1) then 
+			self.currentPosition = self.currentPosition + progress
+			self.achievedState = nil
+			self:ForAll(SetToProgressAlpha, self.currentPosition)
 
-		self.inStartUpLockdown = nil
-		self.totalElapsedStartDelay = 0
-		self.totalElapsed = 0
-		self.totalElapsedIn = 0
-		self.totalElapsedOut = 0
-		self.totalDurationIn = .15
-		self.totalDurationOut = .75
-		self.achievedState = "peril"
-
-		Frame:SetScript("OnUpdate", nil)
-		Frame:SetScript("OnAttributeChanged", function(_, ...) return LibFader:OnAttributeChanged(...) end)
-
-		-- Fire off a fake attribute change to initiate fade events
-		self:OnAttributeChanged("state-fade", SecureCmdOptionParse(DRIVER))
-
-		return 
-	end 
-
-	self.totalElapsed = self.totalElapsed + self.elapsed
-
-	-- iterate objects and set alphas 
-	if (self.targetState == "peril") then 
-		if (self.totalElapsed >= self.totalDurationIn) then 
-			self.elapsed = 0
-			self.totalElapsed = 0
-			self.totalElapsedIn = 0
-			self.achievedState = self.targetState
+		else 
+			self.currentPosition = 1
+			self.achievedState = "peril"
 			self:ForAll(SetToDefaultAlpha)
-			Frame:SetScript("OnUpdate", nil)
-			return 
-		else
-			self.progressIn = self.totalElapsed / self.totalDurationIn
-			self:ForAll(SetToProgressAlpha, self.progressIn)
-		end 
-	else
-		self:CheckMouse()
 
-		-- Temporary forced peril state
-		if Data.mouseOver then 
-
-		end 
-
-		if (self.totalElapsed >= self.totalDurationOut) then 
-			self.elapsed = 0
-			self.totalElapsed = 0
-			self.totalElapsedOut = 0
-			self.achievedState = self.targetState
-			self:ForAll(SetToZeroAlpha)
-			return 
-		else
-			if (self.achievedState ~= "safe") then 
-				self.progressOut = 1 - self.totalElapsed / self.totalDurationOut
-				self:ForAll(SetToProgressAlpha, self.progressOut)
+			if primary then 
+				self.frame:SetScript("OnUpdate", nil)
 			end 
+		end 
+	else 
+		if ((self.currentPosition - progress) > 0) then 
+			self.currentPosition = self.currentPosition - progress
+			self.achievedState = nil
+			self:ForAll(SetToProgressAlpha, self.currentPosition)
+		else 
+			self.currentPosition = 0
+			self.achievedState = "safe"
+			self:ForAll(SetToZeroAlpha)
 		end 
 	end 
 
@@ -443,7 +431,7 @@ end
 
 local embedMethods = {
 	RegisterObjectFade = true,
-	UnregisterObjectFade = true, 
+	UnregisterObjectFade = true
 }
 
 LibFader.Embed = function(self, target)
@@ -459,10 +447,10 @@ for target in pairs(LibFader.embeds) do
 	LibFader:Embed(target)
 end
 
-Frame:SetScript("OnUpdate", nil)
-Frame:SetScript("OnAttributeChanged", nil)
+LibFader.frame:SetScript("OnUpdate", nil)
+LibFader.frame:SetScript("OnAttributeChanged", nil)
 
 LibFader:RegisterEvent("PLAYER_ENTERING_WORLD", "OnEvent")
 
-UnregisterAttributeDriver(Frame, "state-fade")
-RegisterAttributeDriver(Frame, "state-fade", DRIVER)
+UnregisterAttributeDriver(LibFader.frame, "state-fade")
+RegisterAttributeDriver(LibFader.frame, "state-fade", DRIVER)
