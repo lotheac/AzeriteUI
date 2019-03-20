2d9eab2e4d94a21727bf4d71f7a3d0bce31af9d1
Lars Norberg <lars@larsnorbergofficial.com>
2019-03-20 10:56:03 +0100

    order and comment the locales better

--- AzeriteUI/locale/enUS.lua
+++ AzeriteUI/locale/enUS.lua
@@ -6,6 +6,8 @@ end
 
 -- General Stuff
 --------------------------------------------
+-- Most of these are inserted into other strings, 
+-- the idea here is to keep them short and simple. 
 L["Enable"] = true 
 L["Disable"] = true 
 L["Enabled"] = "|cff00aa00Enabled|r"
@@ -14,13 +16,9 @@ L["<Left-Click>"] = true
 L["<Middle-Click>"] = true
 L["<Right-Click>"] = true
 
--- Core Messages
---------------------------------------------
-L["Debug Mode is active."] = true
-L["Type /debug to toggle console visibility!"] = true
-
 -- Clock & Time Settings
 --------------------------------------------
+-- These are shown in tooltips
 L["New Event!"] = true
 L["New Mail!"] = true
 L["%s to toggle calendar."] = true
@@ -35,6 +33,7 @@ L["Now using military (24-hour) time."] = true
 
 -- Network & Performance Information
 --------------------------------------------
+-- These are shown in tooltips
 L["Network Stats"] = true
 L["World latency:"] = true
 L["This is the latency of the world server, and affects casting, crafting, interaction with other players and NPCs. This is the value that decides how delayed your combat actions are."] = true 
@@ -43,6 +42,7 @@ L["This is the latency of the home server, which affects things like chat, guild
 
 -- XP, Honor & Artifact Bars
 --------------------------------------------
+-- These are shown in tooltips
 L["Normal"] = true
 L["Rested"] = true
 L["Resting"] = true
@@ -55,15 +55,22 @@ L["%s of normal experience gained from monsters."] = true
 L["You must rest for %s additional hours to become fully rested."] = true
 L["You must rest for %s additional minutes to become fully rested."] = true
 L["You should rest at an Inn."] = true
+L["Sticky Minimap bars enabled."] = true
+L["Sticky Minimap bars disabled."] = true
+
+-- These are displayed within the circular minimap bar frame, 
+-- and must be very short, or we'll have an ugly overflow going. 
+L["to level %s"] = true 
+L["to %s"] = true
+L["to next trait"] = true
+
+-- Try to keep the following fairly short, as they should
+-- ideally be shown on a single line in the tooltip, 
+-- even with the "<Right-Click>" and similar texts inserted.
 L["%s to toggle Artifact Window>"] = true
 L["%s to toggle Honor Talents Window>"] = true
 L["%s to disable sticky bars."] = true 
 L["%s to enable sticky bars."] = true 
-L["Sticky Minimap bars enabled."] = true
-L["Sticky Minimap bars disabled."] = true
-L["to level %s"] = true 
-L["to %s"] = true
-L["to next trait"] = true
 
 -- Config & Micro Menu
 --------------------------------------------
@@ -72,21 +79,27 @@ L["to next trait"] = true
 --  while making sure the end result still is personalized to the addon.
 L["Main Menu"] = ADDON
 L["Click here to get access to game panels."] = "Click here to get access to the various in-game windows such as the character paperdoll, spellbook, talents and similar, or to change various settings for the actionbars."
+
+-- These should be fairly short to fit in a single line without 
+-- having the tooltip grow to very high widths. 
 L["%s to toggle Blizzard Menu."] = "%s to toggle Blizzard Micro Menu."
 L["%s to toggle Options Menu."] = "%s to toggle "..ADDON.." Options Menu."
 L["%s to toggle your Bags."] = true
 
 -- Config Menu
+--------------------------------------------
+-- Remember that these shall fit on a button, 
+-- so they can't be that long. 
+-- You don't need a full description here. 
 L["Debug Mode"] = true 
-L["Debug Console: %s"] = true 
+L["Debug Console"] = true 
 L["Load Console"] = true
 L["Unload Console"] = true
 L["Reload UI"] = true
-
 L["ActionBars"] = true
-L["Bind Mode: %s"] = true
-L["Cast on Down: %s"] = true
-L["Button Lock: %s"] = true
+L["Bind Mode"] = true
+L["Cast on Down"] = true
+L["Button Lock"] = true
 L["More Buttons"] = true
 L["No Extra Buttons"] = true
 L["+%.0f Buttons"] = true
@@ -95,32 +108,29 @@ L["MouseOver"] = true
 L["MouseOver + Combat"] = true
 L["Always Visible"] = true
 L["Stance Bar"] = true
+L["Pet Bar"] = true
+L["UnitFrames"] = true
+L["Party Frames"] = true
+L["Raid Frames"] = true
+L["PvP Frames"] = true
+L["HUD"] = true
+L["Alerts"] = true
+L["TalkingHead"] = true
+L["NamePlates"] = true
+L["Auras"] = true
+L["Player"] = true
+L["Enemies"] = true 
+L["Friends"] = true
+L["Explorer Mode"] = true
+L["Player Fading"] = true
+L["Tracker Fading"] = true
+L["Healer Mode"] = true 
+
+-- Menu button tooltips, not actually used at the moment. 
 L["Click to enable the Stance Bar."] = true
 L["Click to disable the Stance Bar."] = true
-L["Pet Bar"] = true
 L["Click to enable the Pet Action Bar."] = true
 L["Click to disable the Pet Action Bar."] = true
-
-L["UnitFrames"] = true
-L["Party Frames: %s"] = true
-L["Raid Frames: %s"] = true
-L["PvP Frames: %s"] = true
-
-L["HUD"] = true
-L["Alerts: %s"] = true
-L["TalkingHead: %s"] = true
-
-L["NamePlates"] = true
-L["Auras: %s"] = true
-L["Player: %s"] = true
-L["Enemies: %s"] = true 
-L["Friends: %s"] = true
-
-L["Explorer Mode"] = true
-L["Player Fading: %s"] = true
-L["Tracker Fading: %s"] = true
-
-L["Healer Mode: %s"] = true 
 
 -- Various Button Tooltips
 --------------------------------------------
@@ -129,13 +139,25 @@ L["%s to dismount."] = true
 
 -- Abbreviations
 --------------------------------------------
+-- This is shown of group frames when the unit 
+-- has low or very low mana. Keep it to 3 letters max! 
 L["oom"] = true -- out of mana
+
+-- These are shown on the minimap compass when 
+-- rotating minimap is enabled. Keep it to single letters!
 L["N"] = true -- compass North
 L["E"] = true -- compass East
 L["S"] = true -- compass South
 L["W"] = true -- compass West
 
 -- Keybind mode
+--------------------------------------------
+-- This is shown in the frame, it is word-wrapped. 
+-- Try to keep the length fairly identical to enUS, though, 
+-- to make sure it fits properly inside the window. 
+L["Hover your mouse over any actionbutton and press a key or a mouse button to bind it. Press the ESC key to clear the current actionbutton's keybinding."] = true
+
+-- These are output to the chat frame. 
 L["Keybinds cannot be changed while engaged in combat."] = true
 L["Keybind changes were discarded because you entered combat."] = true
 L["Keybind changes were saved."] = true
@@ -144,69 +166,3 @@ L["No keybinds were changed."] = true
 L["No keybinds set."] = true
 L["%s is now unbound."] = true
 L["%s is now bound to %s"] = true
-
--- Keybinds (visible on the actionbuttons)
-L["Alt"] = "A"
-L["Left Alt"] = "LA"
-L["Right Alt"] = "RA"
-L["Ctrl"] = "C"
-L["Left Ctrl"] = "LC"
-L["Right Ctrl"] = "RC"
-L["Shift"] = "S"
-L["Left Shift"] = "LS"
-L["Right Shift"] = "RS"
-L["NumPad"] = "" -- "N"
-L["Backspace"] = "BS"
-L["Button1"] = "B1"
-L["Button2"] = "B2"
-L["Button3"] = "B3"
-L["Button4"] = "B4"
-L["Button5"] = "B5"
-L["Button6"] = "B6"
-L["Button7"] = "B7"
-L["Button8"] = "B8"
-L["Button9"] = "B9"
-L["Button10"] = "B10"
-L["Button11"] = "B11"
-L["Button12"] = "B12"
-L["Button13"] = "B13"
-L["Button14"] = "B14"
-L["Button15"] = "B15"
-L["Button16"] = "B16"
-L["Button17"] = "B17"
-L["Button18"] = "B18"
-L["Button19"] = "B19"
-L["Button20"] = "B20"
-L["Button21"] = "B21"
-L["Button22"] = "B22"
-L["Button23"] = "B23"
-L["Button24"] = "B24"
-L["Button25"] = "B25"
-L["Button26"] = "B26"
-L["Button27"] = "B27"
-L["Button28"] = "B28"
-L["Button29"] = "B29"
-L["Button30"] = "B30"
-L["Button31"] = "B31"
-L["Capslock"] = "Cp"
-L["Clear"] = "Cl"
-L["Delete"] = "Del"
-L["End"] = "End"
-L["Enter"] = "Ent"
-L["Return"] = "Ret"
-L["Home"] = "Hm"
-L["Insert"] = "Ins"
-L["Help"] = "Hlp"
-L["Mouse Wheel Down"] = "WD"
-L["Mouse Wheel Up"] = "WU"
-L["Num Lock"] = "NL"
-L["Page Down"] = "PD"
-L["Page Up"] = "PU"
-L["Print Screen"] = "Prt"
-L["Scroll Lock"] = "SL"
-L["Spacebar"] = "Sp"
-L["Tab"] = "Tb"
-L["Down Arrow"] = "Dn"
-L["Left Arrow"] = "Lf"
-L["Right Arrow"] = "Rt"
-L["Up Arrow"] = "Up"
