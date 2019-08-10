0c5419cd0860bd2f3f9d4daa51f3cfc13265e133
Lars Norberg <gitkraken@larsnorbergofficial.com>
2019-08-10 23:00:40 +0200

    fix minimap blob alphas

--- AzeriteUI/back-end/libraries/minimap.lua
+++ AzeriteUI/back-end/libraries/minimap.lua
@@ -8,8 +8,6 @@ end
 local LibClientBuild = CogWheel("LibClientBuild")
 assert(LibClientBuild, "LibMinimap requires LibClientBuild to be loaded.")
 
-local IS_CLASSIC = LibClientBuild
-
 local LibMessage = CogWheel("LibMessage")
 assert(LibMessage, "LibMinimap requires LibMessage to be loaded.")
 
@@ -707,10 +705,6 @@ end
 -- These "alpha" values range from 0 to 255, for some obscure reason,
 -- so a value of 127 would be 127/255 ≃ 0.5ish in the normal API.
 LibMinimap.SetMinimapQuestBlobAlpha = function(self, blobInside, blobOutside, ringOutside, ringInside)
-	if IS_CLASSIC then 
-		return 
-	end 
-
 	check(blobInside, 1, "number")
 	check(blobOutside, 2, "number")
 	check(ringOutside, 3, "number")
@@ -754,10 +748,6 @@ end
 
 -- Set all blob values at once
 LibMinimap.SetMinimapBlobAlpha = function(self, blobInside, blobOutside, ringOutside, ringInside)
-	if IS_CLASSIC then 
-		return 
-	end 
-	
 	check(blobInside, 1, "number")
 	check(blobOutside, 2, "number")
 	check(ringOutside, 3, "number")
